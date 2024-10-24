b3e0c66bcbeb4edccabb108b619f61776f3bb6df
MilenaNovka33 <130380764+MilenaNovka33@users.noreply.github.com>
2024-10-24 17:17:58 -0300

    Add files via upload

---
+++ Node-JS-Basico1/index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+console.dir(http)
+/*
+const arr = [1, 5, 6, 8, 12, 32, 3]
+
+console.table(arr)
+console.dir(arr)
+console.error("Error!")
+console.warning("Attemptino")
+console.info(arr)
+console.time()
+
+console.log(arr)
+*/ 
